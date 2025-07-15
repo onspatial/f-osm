6ec16081b622c6f4a3b17479430d80655420dd66
amirih <hossein.amiri.enterprise@gmail.com>
2025-07-15 11:04:42 -0400

    update

--- code/datacollection/foursquare.py
+++ code/datacollection/foursquare.py
@@ -1,51 +1,47 @@
 import pandas
 import pyarrow.parquet as pyarrow_parquet
-import pyarrow
-import pyarrow.fs as pyarrow_fs
-import pyarrow.dataset as pyarrow_dataset
-import pyarrow.csv as pyarrow_csv
-import pyarrow.json as pyarrow_json
-import pyarrow.feather as pyarrow_feather
-import pyarrow.ipc as pyarrow_ipc
 import os
-import sys
+import time
+def print_time(*args, **kwargs):
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    print(f"[{current_time}]: ", *args, **kwargs)
 
 
 def get_raw_data_df(raw_data_path="data/foursquare.csv", refresh=False):
     if os.path.exists(raw_data_path) and not refresh:
-        print(f"File {raw_data_path} already exists. Loading existing data.")
-        raw_data_df = pandas.read_csv(raw_data_path, low_memory=False, dtype=str)
+        print_time(f"File {raw_data_path} already exists. Loading existing data.")
+        raw_data_df = pandas.read_csv(raw_data_path,  dtype=str, encoding='utf-8', engine='python')
     else:
         raw_data_list = []
         for i in range(100):
             csv_path = f"data/converted/places/places-{i:05d}.csv"
             if os.path.exists(csv_path) and not refresh:
-                print(f"File {csv_path} already exists... loading existing data.")
-                table_df = pandas.read_csv(csv_path, low_memory=False)
+                print_time(f"File {csv_path} already exists... loading existing data.")
+                table_df = pandas.read_csv(csv_path,  dtype=str, encoding='utf-8',engine='python')
             else:
-                print(f"Converting file {i} to {csv_path}...")
+                print_time(f"Converting file {i} to {csv_path}...")
                 table_pq = pyarrow_parquet.read_table(f"data/downloaded/places/places-{i:05d}.zstd.parquet")
+                table_pq = table_pq.drop(["geom"])
                 table_df = table_pq.to_pandas()
-                table_df.to_csv(csv_path, index=False)
+                table_df.to_csv(csv_path, index=False, encoding='utf-8')
 
-            table_df = table_df.drop(columns=["geom"])
             raw_data_list.append(table_df)
         raw_data_df = pandas.concat(raw_data_list, ignore_index=True)
-        raw_data_df.to_csv(raw_data_path, index=False)
-    print(f"Loaded {len(raw_data_df)} rows and {len(raw_data_df.columns)} columns from {raw_data_path}")
-    print(raw_data_df.head())
+        raw_data_df.to_csv(raw_data_path, index=False, encoding='utf-8')
+    print_time(f"Loaded {len(raw_data_df)} rows and {len(raw_data_df.columns)} columns from {raw_data_path}")
+    print_time(raw_data_df.head())
     return raw_data_df
 
 
 def download_foursquare():
     if os.path.exists("data/downloaded/places/places-00000.zstd.parquet"):
-        print("Data already downloaded. Skipping download.")
+        print_time("Data already downloaded. Skipping download.")
         return
-    print("Downloading the parquet files...")
+    print_time("Downloading the parquet files...")
     # os.system("sudo dnf install -y awscli2")
     os.system("aws s3 cp --no-sign s3://fsq-os-places-us-east-1/release/dt=2025-07-08/places/parquet data/downloaded/places/ --recursive")
     os.system("aws s3 cp --no-sign s3://fsq-os-places-us-east-1/release/dt=2025-07-08/categories/parquet data/downloaded/categories/ --recursive")
-    print("Download complete.")
+    print_time("Download complete.")
 
 def make_directories():
     try:
@@ -67,7 +63,7 @@ def initialize():
 def save_clean_data(raw_data_df, clean_data_path):
     clean_data_df = raw_data_df.copy()
     clean_data_df.columns = [f"fsq_{col}" if not col.startswith("fsq_") else col for col in clean_data_df.columns]
-    print(f"Cleaned column names: {clean_data_df.columns.tolist()}")
+    print_time(f"Cleaned column names: {clean_data_df.columns.tolist()}")
     clean_data_df.to_csv(clean_data_path, index=False)
 
 if __name__ == "__main__":
@@ -76,5 +72,3 @@ if __name__ == "__main__":
     clean_data_path = "data/foursquare_clean.csv"
     raw_data_df = get_raw_data_df()
     save_clean_data(raw_data_df, clean_data_path)
-    
-    
--- code/datacollection/fsq_import.sql
+++ code/datacollection/fsq_import.sql
@@ -1,7 +1,39 @@
 
 
  CREATE TABLE foursquare (
-      fsq_place_id        text,  name                text,  latitude            text,  longitude           text,  address             text,  locality            text,  region              text,  postcode            text,  admin_region        text,  post_town           text,  po_box              text,  country             text,  date_created        text,  date_refreshed      text,  date_closed         text,  tel                 text,  website             text,  email               text,  facebook_id         text,  instagram           text,  twitter             text,  fsq_category_ids    text,  fsq_category_labels text,  placemaker_url      text,  geom                text,  bbox                text);
+      fsq_place_id        text,
+      name                text,
+      latitude            text,
+      longitude           text,
+      address             text,
+      locality            text,
+      region              text,
+      postcode            text,
+      admin_region        text,
+      post_town           text,
+      po_box              text,
+      country             text,
+      date_created        text,
+      date_refreshed      text,
+      date_closed         text,
+      tel                 text,
+      website             text,
+      email               text,
+      facebook_id         text,
+      instagram           text,
+      twitter             text,
+      fsq_category_ids    text,
+      fsq_category_labels text,
+      placemaker_url      text,
+      bbox                text
+);
 
-\copy foursquare  FROM 'data/raw_data.csv'  WITH (    FORMAT csv,    HEADER,    DELIMITER ',',    QUOTE '"',    ESCAPE '"',    NULL ''  );
+\copy foursquare  FROM 'data/raw_data.csv'  WITH (    
+      FORMAT csv,
+      HEADER,
+      DELIMITER ',',
+      QUOTE '"',
+      ESCAPE '"',
+      NULL ''  
+);
 
